219fb5b02e9099f4e7a98a2e53bb3bad78fc13eb
sandyseals12 <129459498+sandyseals12@users.noreply.github.com>
2023-05-25 18:00:49 -0400

    Add files via upload
    need help with last df and map but im pretty sure the answer is in the api docs turning in to get off my plate

---
+++ python-api-challenge/Vacationpy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "4899b29866bb6cf9bac5e311669cc627"
+
+# Geoapify API Key
+geoapify_key= "5dfc05e4b9c84d3b9ead3886393a23c5"
---
+++ python-api-challenge/Weatherpy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key= "4899b29866bb6cf9bac5e311669cc627"
+
+# Geoapify API Key
+geoapify_key = "YOUR KEY HERE"
